3d0613216f2ad028987370722b3a7d043d4339a0
ChrisWen <chriswen96.com>
2022-08-09 11:36:07 +0800

    uni-app-num

---
+++ pages/index/index.vue
@@ -0,0 +1,85 @@
+<template>
+	<view class="content">
+
+		<view class="panel">
+			<text>{{num}}</text>
+		</view>
+
+		<view class="button-area">
+			<button v-show="isLoading" @click="start"> 开始抽奖 </button>
+			<button v-show="!isLoading" @click="stop"> 结束抽奖 </button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				num: 8,
+				min:0,//含最小值
+				max:100,//不含最大值
+				interval:100,//毫秒
+				isLoading: true,
+				clock:null,
+
+			}
+		},
+		onLoad() {
+
+		},
+		onShow(){
+			let setting = uni.getStorageSync('setting') || { max:100, min:0, interval:20 }
+			this.max = setting.max
+			this.min = setting.min
+			this.interval = setting.interval
+		},
+		methods: {
+			start(){
+				this.isLoading = false
+				this.clock = setInterval(()=> {
+					this.num = this.getRandomNum()
+				},this.interval)
+			},
+			stop(){
+				clearInterval(this.clock)
+				this.isLoading = true
+			},
+			
+			getRandomNum(){
+				return parseInt(this.min) + Math.floor(Math.random()*(parseInt(this.max) - parseInt(this.min)))
+			},
+
+		}
+	}
+</script>
+
+<style lang="scss">
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+	
+	.panel {
+		width: 60vw;
+		height: 30vh;
+		margin: 10vh;
+		border-radius: 28rpx;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		border: 1rpx solid $uni-border-color;
+		box-shadow:0px 0px 15px 0px rgba(0,0,0,0.5);
+		text {
+			font-size: 180rpx;
+		}
+	}
+	
+	.button-area{
+
+	}
+
+
+</style>
---
+++ pages/setting/setting.vue
@@ -0,0 +1,74 @@
+<template>
+
+	<view class="content">
+		<view class="list">
+		<view class="uni-form-item uni-column">
+			<view class="title">最大值（数字）</view>
+			<input class="uni-input" v-model="max" type="number" placeholder="最大值" />
+		</view>
+		<view class="uni-form-item uni-column">
+			<view class="title">最小值（数字）</view>
+			<input class="uni-input"  v-model="min" type="number" placeholder="最小值" />
+		</view>
+		<view class="uni-form-item uni-column">
+			<view class="title">时间间隔（毫秒）</view>
+			<input class="uni-input"  v-model="interval" type="number" placeholder="时间间隔" />
+		</view>
+		</view>
+		
+		<view class="button-area">
+			<button @click="onHold">保存</button>
+		</view>
+	</view>
+
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				max:100,
+				min:0,
+				interval:20,
+			}
+		},
+		created(){
+			let setting = uni.getStorageSync('setting') || { max:100, min:0, interval:20 }
+			this.max = setting.max
+			this.min = setting.min
+			this.interval = setting.interval
+		},
+		methods: {
+			onHold(){
+				let setting = { max:this.max, min:this.min, interval:this.interval }
+				uni.setStorageSync('setting', setting);
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.uni-form-item {
+		margin: 5vh;
+	}
+
+	.title {
+		font-size: 30rpx;
+		font-weight: 600;
+		margin-bottom: 1vh;
+	}
+
+	.uni-input {
+		border-bottom: 1px solid $uni-border-color;
+		font-size: 26rpx;
+		margin-top: 10rpx;
+	}
+	.button-area {
+		margin-top: 10vh;
+		display: flex;
+		justify-content: center;
+		button {
+			width: 30vw;
+		}
+	}
+</style>
